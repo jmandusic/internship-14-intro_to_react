16f78f99aa21b50e5b8f1a94806ecf341b5a218d
jmandusic <74503867+jmandusic@users.noreply.github.com>
2021-03-11 21:58:30 +0100

    Added TODO form, needs more styling

---
+++ todo-app/src/components/TaskForm.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+
+const TaskForm = ({ addTask }) => {
+  const [formState, setFormState] = useState({
+    id: 1,
+    title: "",
+    description: "",
+    priority: "Low",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setFormState((prevState) => ({ ...prevState, title: e.target.value }));
+  };
+
+  const descriptionChangeHandler = (e) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      description: e.target.value,
+    }));
+  };
+
+  const priorityChangeHandler = (e) => {
+    setFormState((prevState) => ({ ...prevState, priority: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formState.title) {
+      setErrorMessage("Title is a required field");
+      return;
+    }
+
+    if (!formState.description) {
+      setErrorMessage("Description is a required field");
+      return;
+    }
+
+    addTask(formState);
+    setFormState((prevState) => ({
+      id: prevState.id + 1,
+      title: "",
+      description: "",
+      priority: "Low",
+    }));
+    setErrorMessage("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="add-todo__form">
+      <h2 className="add-todo__header">Add todos</h2>
+
+      <input
+        value={formState.title}
+        name="title"
+        placeholder="Title"
+        className="add-todo__title"
+        onChange={titleChangeHandler}
+      />
+
+      <textarea
+        value={formState.description}
+        name="description"
+        placeholder="Description"
+        className="add-todo__description"
+        onChange={descriptionChangeHandler}
+      ></textarea>
+
+      <select
+        value={formState.priority}
+        className="add-todo__priority"
+        onChange={priorityChangeHandler}
+      >
+        <option>Low</option>
+        <option>Medium</option>
+        <option>High</option>
+      </select>
+
+      <button type="submit" className="add-todo__submit-button">
+        SUBMIT
+      </button>
+
+      {errorMessage && (
+        <span className="form__erro-message">ERROR: {errorMessage}</span>
+      )}
+    </form>
+  );
+};
+
+export default TaskForm;
